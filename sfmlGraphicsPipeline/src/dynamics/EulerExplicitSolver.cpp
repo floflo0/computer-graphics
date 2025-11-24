2cd2149ed2b58ff11d9bb8951ef365f9bd653640
Floris Bartra <98590668+floflo0@users.noreply.github.com>
2025-11-24 08:10:06 +0100

    Fix pratical 5 (#11)

--- sfmlGraphicsPipeline/src/dynamics/EulerExplicitSolver.cpp
+++ sfmlGraphicsPipeline/src/dynamics/EulerExplicitSolver.cpp
@@ -1,28 +1,18 @@
-#include "./../../include/dynamics/EulerExplicitSolver.hpp"
-#define GRAVITY = 9.806 65 // Constant for gravity
+#include <dynamics/EulerExplicitSolver.hpp>
 
-EulerExplicitSolver::EulerExplicitSolver()
-{
+EulerExplicitSolver::EulerExplicitSolver() {}
 
-}
+EulerExplicitSolver::~EulerExplicitSolver() {}
 
-EulerExplicitSolver::~EulerExplicitSolver()
-{
+void EulerExplicitSolver::do_solve(const float &dt, std::vector<ParticlePtr>& particles) {
+    for(ParticlePtr p : particles) {
+        if(!p->isFixed()) {
+            // Functions to use:
+            // Particle::getPosition(), Particle::getVelocity(),
+            // Particle::getMass(), Particle::getForce(),
+            // Particle::setPosition(), Particle::setVelocity()
 
-}
-
-void EulerExplicitSolver::do_solve(const float& dt, std::vector<ParticlePtr>& particles)
-{
-    for(ParticlePtr p : particles)
-    {
-        if(!p->isFixed())
-        {
-            //TODO: Implement explicit euler solver
-            //Functions to use:
-            //Particle::getPosition(), Particle::getVelocity(), Particle::getMass(), Particle::getForce()
-            //Particle::setPosition(), Particle::setVelocity()
-
-            p->setVelocity(p->getVelocity() + (1/p->getMass()) * p->getForce() * dt);
+            p->setVelocity(p->getVelocity() + (1.0f / p->getMass()) * p->getForce() * dt);
             p->setPosition(p->getPosition() + p->getVelocity() * dt);
         }
     }
